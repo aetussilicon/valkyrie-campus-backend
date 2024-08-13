2f703bb0d6be548dfaf6f0a735147a4df6612fed
Artur Oliveira <arturgaollidev@outlook.com>
2024-08-13 19:41:58 -0300

    Commit Inicial

---
+++ src/main/java/br/com/valkyrie/campus/ValkyrieCampusApplication.java
@@ -0,0 +1,13 @@
+package br.com.valkyrie.campus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ValkyrieCampusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ValkyrieCampusApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/valkyrie/campus/ValkyrieCampusApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.valkyrie.campus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ValkyrieCampusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
