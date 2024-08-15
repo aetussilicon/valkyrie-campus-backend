c0883095755f5f93236449e9fea8db44da0c6be4
Artur Oliveira <arturgaollidev@outlook.com>
2024-08-15 15:22:53 -0300

    Criado DTO, Mapper e repository para novas answer
    - Criado DTO `NewAnswerDto` para postagem de novas respostas.
    - Criado o mapeamento desse Dto para a entidade.
    - Criado repositório para as respostas.

---
+++ src/main/java/br/com/valkyrie/campus/model/dtos/NewAnswerDto.java
@@ -0,0 +1,25 @@
+package br.com.valkyrie.campus.model.dtos;
+
+import br.com.valkyrie.campus.model.entities.Posts;
+import br.com.valkyrie.campus.model.entities.Users;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Date;
+import java.util.UUID;
+
+@Getter
+@Setter
+public class NewAnswerDto {
+    private UUID answerId;
+    private Posts post;
+    private UUID postId;
+    private Users answerBy;
+    private UUID answerByUserId;
+    private String usertag;
+    private String answerContent;
+    private int upvote;
+    private int downvote;
+    private Date createdDate;
+    private Date lastUpdatedDate;
+}
---
+++ src/main/java/br/com/valkyrie/campus/model/mappers/AnswerMapper.java
@@ -0,0 +1,19 @@
+package br.com.valkyrie.campus.model.mappers;
+
+import br.com.valkyrie.campus.model.dtos.NewAnswerDto;
+import br.com.valkyrie.campus.model.entities.Answer;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.factory.Mappers;
+
+import java.util.UUID;
+
+@Mapper(componentModel = "spring", imports = UUID.class)
+public interface AnswerMapper {
+    AnswerMapper INSTANCE = Mappers.getMapper(AnswerMapper.class);
+
+    // AnswerDto to Answer
+    @Mapping(target = "accepted", ignore = true)
+    @Mapping(target = "answerId", expression = "java(UUID.randomUUID())")
+    Answer answerDtoToModel(NewAnswerDto dto);
+}
